eb8e0837943c22a3408a1c917ba65cda34e83b15
yilm01 <philosophia268@gmail.com>
2022-10-07 23:49:10 +0900

    react-router-domを新しくinstallしてpackage.jsonに追加されたので再度環境構築してください。componentsにrendering先が入ってます。

---
+++ docker-react-app/reactapp/src/App.jsx
@@ -0,0 +1,31 @@
+import {BrowserRouter,Link, Route, Routes} from "react-router-dom";
+
+import {Home} from "./components/Home";
+import {Page1} from "./components/Page1";
+import {Page2} from "./components/Page2";
+import {Page3} from "./components/Page3";
+
+function App(){
+  return(
+    <BrowserRouter>
+      <div className="App">
+        <Link to="/">Home</Link>
+        <br />
+        <Link to="/page1">Page1</Link>
+        <br />
+        <Link to="/page2">Page2</Link>
+        <br />
+        <Link to="/page3">Page3</Link>
+        <br />
+        <Routes>
+        <Route  path="/" element={<Home />}/>
+        <Route  path="/page1" element={ <Page1 />}/>
+        <Route  path="/page2" element={<Page2 />}/> 
+        <Route  path="/page3" element={<Page3 />}/> 
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
