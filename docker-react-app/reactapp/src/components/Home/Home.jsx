5a6b758cb8843a8a5d12b0a86f97658991266908
Basstubass <0130tubasa@gmail.com>
2022-10-10 13:41:48 +0900

    add header

--- docker-react-app/reactapp/src/components/Home/Home.jsx
+++ docker-react-app/reactapp/src/components/Home/Home.jsx
@@ -13,16 +13,28 @@ export const Home=()=>{
         {/* メインはスクロール処理で表示、非表示 */}
         <div className='main'>
           <div className='profile_aria'>
-          <div className='profile_img'>
-            <img src={home_image} alt="" />
-          </div>
-            <div className='profile_textcontents'>
-              <h1>Hajime Kawanami</h1>
-              <h2>川波 肇</h2>
-              <p>国立研究開発法人産業技術総合研究所</p>
-              <p>触媒化学融合研究センター</p>
-              <p>官能基変換チーム 上級主任研究員</p>
+            <div className='profile_textcontents profile'>
+              <h1> Hajime Kawanami</h1>
+              <h2> 川波 肇</h2>
+              <p> ー 国立研究開発法人産業技術総合研究所</p>
+              <p> ー 触媒化学融合研究センター</p>
+              <p> ー 官能基変換チーム 上級主任研究員</p>
             </div>
+
+            <div className='profile_img profile'>
+              <img src={home_image} alt="" />
+            </div>
+
+            {/* スクロールアニメーション */}
+            <div class="scrolldown4"><span>Scroll</span></div>
+            <div id="container">
+            <p>このエリアがスクロールすると上にかぶさります</p> 
+            </div> 
+            {/* スクロールアニメーション */}
+
+
+
+
           </div>
         </div>
       </main>
