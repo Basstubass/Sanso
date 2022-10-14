816b633ccfb1de144b112bacacc2c38281fac99a
Basstubass <0130tubasa@gmail.com>
2022-10-15 01:33:11 +0900

    Newsページの更新と全体と調整

--- docker-react-app/reactapp/src/components/Members/Page2.jsx
+++ docker-react-app/reactapp/src/components/Members/Page2.jsx
@@ -13,6 +13,7 @@ export const Page2=()=>{
         <div className='owners_aria'>
           <h1 className='head-border'> 提督 </h1>
         </div>
+        {/* 更新するエリア */}
           <div className='member_profile_aria'>
             <div className='member_profile_textcontents profile'>
                 <h1> Hajime Kawanami</h1>
@@ -29,11 +30,49 @@ export const Page2=()=>{
                 <img src={home_image} alt="" />
             </div>
           </div>
-        <div className='owners_aria'>
+        {/* 更新するエリア */}
+
+        <div className='members_aria'>
           <h1 className='head-border'> 研究員 </h1>
         </div>
         
-        
+
+        {/* 更新するエリア */}
+        <div className='member_profile_aria'>
+            <div className='member_profile_textcontents profile'>
+                <h1> Hajime Kawanami</h1>
+                <h2> 川波 肇</h2>
+                <p> ー 国立研究開発法人産業技術総合研究所</p>
+                <p> ー 触媒化学融合研究センター</p>
+                <p> ー 官能基変換チーム 上級主任研究員</p>
+            </div>
+            <h3>Comments</h3>
+              <div className='members_comments' id="story" name="story"rows="5" cols="33">
+                <p>It was a dark and stormy night...wwwwwwwwwwwwwwwwwwwwwdsdasddssdfsdfsdfwfdfw sdfsadfsdfsdfsdfsdfsdfdf</p>
+              </div>
+            <div className='member_profile_img profile'>
+                <img src={home_image} alt="" />
+            </div>
+          </div>
+        {/* 更新するエリア */}
+        <div className='member_profile_aria'>
+            <div className='member_profile_textcontents profile'>
+                <h1> Hajime Kawanami</h1>
+                <h2> 川波 肇</h2>
+                <p> ー 国立研究開発法人産業技術総合研究所</p>
+                <p> ー 触媒化学融合研究センター</p>
+                <p> ー 官能基変換チーム 上級主任研究員</p>
+            </div>
+            <h3>Comments</h3>
+              <div className='members_comments' id="story" name="story"rows="5" cols="33">
+                <p>It was a dark and stormy night...wwwwwwwwwwwwwwwwwwwwwdsdasddssdfsdfsdfwfdfw sdfsadfsdfsdfsdfsdfsdfdf</p>
+              </div>
+            <div className='member_profile_img profile'>
+                <img src={home_image} alt="" />
+            </div>
+          </div>
+
+
       </div>
     </main>
   );
--- docker-react-app/reactapp/src/components/News/Page3.jsx
+++ docker-react-app/reactapp/src/components/News/Page3.jsx
@@ -1,8 +1,87 @@
+import './news.css';
 export const Page3=()=>{
   return (
-    <div>
-    <div><h1>Page3</h1></div>
-    <div>うつてっるうう？</div>
-    </div>
+    <main>
+      <div className='main'>
+        <div className='news_animation_aria'>
+          <h1>News</h1>
+          <h2>お知らせ</h2>
+        </div>
+
+
+
+
+        <div className='news'>
+            <hr width="80%"></hr>
+            <div className='news_aria news_text'>
+              <h1>News</h1>
+              <p>お知らせ</p>
+            </div>
+            <div className='radiobutton news_aria'>
+              <input id="radio1"  name="hoge" type="radio" value="ラジオボタン1"/>
+              <label for="radio1">News</label>
+            </div>
+            <div className='radiobutton news_aria'>
+              <input id="radio1"  name="hoge" type="radio" value="ラジオボタン1"/>
+              <label for="radio1">Topics</label>
+            </div>
+            
+            {/* ニュースとトピックはボタンにて制御 */}
+            {/* データベースより取得、news_textcontentsを生成 */}
+            <div className='news_topics_aria'>
+
+               {/* 更新するエリア */}
+              <div className='news_textcontents'>
+                <p>2001/01/30</p>
+                <p>whats news enable hello world hello world</p>
+                <hr width="60%"></hr>
+              </div>
+               {/* 更新するエリア */}
+              <div className='news_textcontents'>
+                <p>2001/01/30</p>
+                <p>whats news enable hello world hello world</p>
+                <hr width="60%"></hr>
+              </div>
+              <div className='news_textcontents'>
+                <p>2001/01/30</p>
+                <p>whats news enable hello world hello world</p>
+                <hr width="60%"></hr>
+              </div> 
+              <div className='news_textcontents'>
+                <p>2001/01/30</p>
+                <p>whats news enable hello world hello world</p>
+                <hr width="60%"></hr>
+              </div> 
+              <div className='news_textcontents'>
+                <p>2001/01/30</p>
+                <p>whats news enable hello world hello world</p>
+                <hr width="60%"></hr>
+              </div> 
+              <div className='news_textcontents'>
+                <p>2001/01/30</p>
+                <p>whats news enable hello world hello world</p>
+                <hr width="60%"></hr>
+              </div> 
+              <div className='news_textcontents'>
+                <p>2001/01/30</p>
+                <p>whats news enable hello world hello world</p>
+                <hr width="60%"></hr>
+              </div> 
+            </div>
+
+            <div className='more_aria'>
+              <a href='/news'>
+                <button className='aria_button'>more</button>
+                <i className='fas fa-angle-right aria_button'></i>
+              </a>
+            </div>
+            
+          </div>
+        
+
+
+
+      </div>
+    </main>
   );
 };
