fbb5857f34d569ecf946545db7d9888595420879
Basstubass <0130tubasa@gmail.com>
2022-10-21 00:04:46 +0900

    firebaseのデータ取得

--- docker-react-app/reactapp/src/components/Project/Page1.jsx
+++ docker-react-app/reactapp/src/components/Project/Page1.jsx
@@ -13,8 +13,7 @@ export const Page1=()=>{
     //データ取得
     const postData = collection(db, "news")
         getDocs(postData).then((snapShot) => {
-        console.log(snapShot.docs.map((doc) => ({...doc.data()})))
-        setPosts(snapShot.docs.map((doc) => ({ ...doc.data() }))) 
+        setPosts(snapShot.docs.map((doc) => ({ ...doc.data() })));
       });
 
       onSnapshot(postData, (post) => {
@@ -40,44 +39,20 @@ export const Page1=()=>{
                 <p>研究紹介</p>
               </div>
           </div>
-
           {/* 更新エリア */}
-          <div className='project_contents'>
+          {post.map((news) => (
+            <div className='project_contents'>
             <div className='project_contents_text project_aria'>
-              <h1>高圧水素製造技術</h1>
-              <p>水素社会に向けて、長年培ってきた高圧技術を駆使し、
-                 ギ酸を水素キャリアとする新しい水素製造技術の開発を行っています。
-                 特に高圧下でもギ酸脱水素化反応に耐えられる触媒開発や、
-                 高圧でも安定した水素を供給できるシステムの開発などを行っています
-                （写真は開発した100MPaの高圧水素製造装置）
-                 将来はギ酸から作った水素で燃料電池車等を走らせるために基盤技術開発も行っています。
-              </p>
+              <h1>{news.title}</h1>
+              <p>{news.title}</p>
             </div>
             <div className='project_contents_img project_aria'>
               <img src={test_img} alt=""></img>
             </div>
             <hr width="90%"></hr>
           </div>
+          ))}
           {/* 更新エリア */}
-          {/* 更新エリア */}
-          <div className='project_contents'>
-            <div className='project_contents_text project_aria'>
-              <h1>高圧水素製造技術</h1>
-              <p>水素社会に向けて、長年培ってきた高圧技術を駆使し、
-                 ギ酸を水素キャリアとする新しい水素製造技術の開発を行っています。
-                 特に高圧下でもギ酸脱水素化反応に耐えられる触媒開発や、
-                 高圧でも安定した水素を供給できるシステムの開発などを行っています
-                （写真は開発した100MPaの高圧水素製造装置）
-                 将来はギ酸から作った水素で燃料電池車等を走らせるために基盤技術開発も行っています。
-              </p>
-            </div>
-            <div className='project_contents_img project_aria'>
-              <img src={test_img} alt=""></img>
-            </div>
-            <hr width="90%"></hr>
-          </div>
-          {/* 更新エリア */}
-
         </div>
       </main>
     
