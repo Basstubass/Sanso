abd75ab549d6f9cd7ed8ce5c00330b2888f26199
Basstubass <0130tubasa@gmail.com>
2022-10-23 21:06:30 +0900

    投稿関数の作成

--- docker-react-app/reactapp/src/components/Other/Page4.jsx
+++ docker-react-app/reactapp/src/components/Other/Page4.jsx
@@ -1,22 +1,34 @@
 import { signInWithPopup } from "firebase/auth";
 import { auth, provider } from "../../firebase";
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 // import { useEffect, useState } from "react";
 
 import { useAuthState } from "react-firebase-hooks/auth";
 import { getDatabase, ref, set } from "firebase/database";
 
-import { doc, setDoc } from "firebase/firestore";
+import { doc, setDoc, Timestamp, collection, addDoc } from "firebase/firestore";
 import { db } from "../../firebase";
 
 export const Page4=()=>{
+
+  const inputRef = useRef();
   // const user = auth.currentUser;
+  //ユーザー追加hooks Stateの多用もっといい方法あるかも
   const [users, setUsers] = useState('')
   const [food, setFoods] = useState('')
-  // let users_data = [users, food]
+  const [comments, setComments] = useState('')
+  //Newsの追加
+  const [title, setTitle] = useState('')
+  const [text, setText] = useState('')
+  //Topicksの追加
+  const [tp_title, setTp_title] = useState('')
+  const [tp_text, setTp_text] = useState('')
 
+
+
+
+  //ログインhooks
   const [user] = useAuthState(auth)
-  // console.log(auth.currentUser.displayName)
 
 
   return (
@@ -31,20 +43,48 @@ export const Page4=()=>{
             <h1>{auth.currentUser.displayName}</h1>
           </div>
           <div className="post_aria">
-            <button onClick={handleonClick_user_AddButton}>追加</button>
 
-            {/* ユーザーの追加 */}
+            <h1>メンバーの追加</h1>
+              {/* ユーザーの追加 */}
             <div>
-            <input value={users} onChange={(event) => setUsers(event.target.value)}/>
+              <input value={users} onChange={(event) => setUsers(event.target.value)}/>
             </div>
-            {/* フードの追加 */}
+              {/* フードの追加 */}
             <div>
-            <input value={food} onChange={(event) => setFoods(event.target.value)}/>
-              <button onClick={() => handleonClick_user_AddButton(users,food)}>フードの追加</button>
+              <input value={food} onChange={(event) => setFoods(event.target.value)}/>
             </div>
-         
+              {/* コメントの追加 */}
+            <div>
+              <input  value={comments} onChange={(event) => setComments(event.target.value)}/>
+                <button onClick={() => handleonClick_user_AddButton(users,food,comments, inputRef)}>メンバーの追加</button>
+            </div>
+
+            <h1>Newsの追加</h1>
+            {/* タイトルの追加 */}
+            <div>
+              <input ref={inputRef} value={title} onChange={(event) => setTitle(event.target.value)}/>
+            </div>
+            {/* テキストの追加 */}
+            <div>
+              <input ref={inputRef} value={text} onChange={(event) => setText(event.target.value)}/>
+              <button onClick={() => handleonClick_News_AddButton(title,text, inputRef)}>News</button>
+            </div>
+
+            <h1>Topicsの追加</h1>
+            {/* タイトルの追加 */}
+            <div>
+              <input ref={inputRef} value={tp_title} onChange={(event) => setTp_title(event.target.value)}/>
+            </div>
+            {/* テキストの追加 */}
+            <div>
+              <input ref={inputRef} value={tp_text} onChange={(event) => setTp_text(event.target.value)}/>
+              <button onClick={() => handleonClick_Topics_AddButton(tp_title,tp_text, inputRef)}>Topics</button>
+            </div>
+
+
+
+
           </div>
-
 
           <SignOutbutton/>
         </>
@@ -87,15 +127,32 @@ function UserInfo(){
   )
 }
 
+////////////////投稿用の関数/////////////
+//ユーザーハンドル
+const handleonClick_user_AddButton = async (users, food, comments) => {
+  await addDoc(collection(db, "users"), {
+    name: users,
+    food: food,
+    comments: comments,
+    times: Timestamp.fromDate(new Date("December 10, 1815")),
+  });
+}
 
-
-////////////////投稿用の関数
-const handleonClick_user_AddButton = async (users, food) => {
-  console.log(users)
-
-
-  await setDoc(doc(db, "users", "1"), {
-    food: users,
-    name: food
+//ニュースハンドル
+const handleonClick_News_AddButton = async (title, text, inputRef) => {
+  await addDoc(collection(db, "news"), {
+    editer: auth.currentUser.displayName,
+    title: title,
+    text: text,
+    times: Timestamp.fromDate(new Date("December 10, 1815")),
+  });
+}
+//トピックハンドル
+const handleonClick_Topics_AddButton = async (tp_title, tp_text, inputRef) => {
+  await addDoc(collection(db, "topic"), {
+    editer: auth.currentUser.displayName,
+    title: tp_title,
+    text: tp_text,
+    times: Timestamp.fromDate(new Date("December 10, 1815")),
   });
 }
