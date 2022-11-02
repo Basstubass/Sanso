7f763e13607528c2cff1daab0ac87a16339eeb4b
yilm01 <philosophia268@gmail.com>
2022-11-02 12:11:02 +0900

    storageから読み込むことはできました

--- docker-react-app/reactapp/src/components/Project/Page1.jsx
+++ docker-react-app/reactapp/src/components/Project/Page1.jsx
@@ -3,11 +3,28 @@ import './project.css';
 import React from "react";
 // import {useCollection} from "react-firebase-hooks/firestore";
 import { useEffect, useState } from "react";
-import { collection, getDocs, onSnapshot} from "firebase/firestore";
+import { collection, getDocs, onSnapshot, query, where} from "firebase/firestore";
 import {getDownloadURL, ref, listAll} from "firebase/storage";
 import { db, storage } from "../../firebase";
 
 export const Page1=()=>{
+
+
+  // storageからURLだけ保存したものをstoreから呼び出している関数
+  const [post, setPosts] = useState([]);
+  useEffect(() => {
+    //データ取得
+    const postData = collection(db, "storage_links")
+        getDocs(postData).then((snapShot) => {
+        setPosts(snapShot.docs.map((doc) => ({ ...doc.data() })));
+      });
+      onSnapshot(postData, (post) => {
+        setPosts(post.doc.map((doc) => ({...doc.data() })));
+      });
+      post.map((value, index)=>(
+        console.log(`1回目のpostの中身${value.link}`)
+      ));
+  },[]);
 
   const[image, setImage]=useState([]);
   const[project, setProject]=useState([]);
@@ -27,9 +44,11 @@ export const Page1=()=>{
   // グローバル配列をおきます。
   const prefixes=[];
   const image_name=[];
+
+  // storageから直接持ってくる関数
   // imgの取得
-  const OutImage=(image_name)=>{
-    console.log("OutImage関数内のimage_name配列の中身: "+image_name);
+  const OutImage=()=>{
+    // console.log("OutImage関数内のimage_name配列の中身: "+image_name);
     const gsReference = ref(
       storage,
       image_name[0],
@@ -48,9 +67,9 @@ export const Page1=()=>{
     });
     res.items.forEach((folderRef)=>{
       image_name.push(folderRef);
-      console.log("関数内でのimage_name配列の中身: "+image_name);
+      // console.log("関数内でのimage_name配列の中身: "+image_name);
     })
-    console.log("listAll関数内でのimage_name配列の中身: "+image_name[0]);
+    // console.log("listAll関数内でのimage_name配列の中身: "+image_name[0]);
     OutImage(image_name);
   }).catch((error)=>{
     console.log("エラーですで");
@@ -79,6 +98,18 @@ export const Page1=()=>{
             <div className='project_contents_text project_aria'>
               <h1>{project.title}</h1>
               <p>{project.text}</p>
+            </div>
+            <div className='project_contents_img project_aria'>
+              <img src={image} alt="" />
+            </div>
+            <hr width="90%"></hr>
+          </div>
+          ))}
+     {post.map((value, index) => (
+            <div className='project_contents' key={index}>
+            <div className='project_contents_text project_aria'>
+              <h1>{value.id}</h1>
+              <p>{value.link}</p>
             </div>
             <div className='project_contents_img project_aria'>
               <img src={image} alt="" />
